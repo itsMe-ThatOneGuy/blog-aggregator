0c0effe57314b1d48de43e3379883f3baf4eabf7
Matthew Smart <matthewsmart13@gmail.com>
2024-11-18 12:57:52 -0600

    Created config package with basic config logic

---
+++ internal/config/gator_config.go
@@ -0,0 +1,78 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	DB   string `json:"db_url"`
+	User string `json:"current_user_name"`
+}
+
+func TestDir() {
+	test, _ := getConfigFilePath()
+
+	fmt.Println(test)
+}
+
+func Read() (Config, error) {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	config := Config{}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return Config{}, errors.New("issue when decoding json file")
+	}
+
+	return config, nil
+}
+
+func (cfg *Config) SetUser(name string) error {
+	cfg.User = name
+	return write(*cfg)
+}
+
+func write(cfg Config) error {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(filePath, os.O_WRONLY, 0644)
+	if err != nil {
+		return errors.New("issue opening file")
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(cfg)
+	if err != nil {
+		return errors.New("issue writing to file")
+	}
+
+	return nil
+}
+
+func getConfigFilePath() (string, error) {
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return "", errors.New("Issue reading file")
+	}
+
+	fullPath := dir + "/.gatorconfig.json"
+
+	return fullPath, nil
+}
