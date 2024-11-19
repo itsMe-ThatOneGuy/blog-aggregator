6839fb73ab279e723ef2c6e540047446a92b42d3
Matthew Smart <matthewsmart13@gmail.com>
2024-11-19 10:12:44 -0600

    Added database pointer to state struct

--- internal/state/gator_state.go
+++ internal/state/gator_state.go
@@ -1,7 +1,11 @@
 package state
 
-import "github.com/itsMe-ThatOneGuy/blog-aggregator/internal/config"
+import (
+	"github.com/itsMe-ThatOneGuy/blog-aggregator/internal/config"
+	"github.com/itsMe-ThatOneGuy/blog-aggregator/internal/database"
+)
 
 type State struct {
+	DB            *database.Queries
 	ConfigPointer *config.Config
 }
