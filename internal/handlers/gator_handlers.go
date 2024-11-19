df5f5640129b2e94579841e47cd514b1e1d79a31
Matthew Smart <matthewsmart13@gmail.com>
2024-11-19 10:11:13 -0600

    Created the user register handler

--- internal/handlers/gator_handlers.go
+++ internal/handlers/gator_handlers.go
@@ -1,10 +1,16 @@
 package handlers
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
+	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/itsMe-ThatOneGuy/blog-aggregator/internal/commands"
+	"github.com/itsMe-ThatOneGuy/blog-aggregator/internal/database"
 	"github.com/itsMe-ThatOneGuy/blog-aggregator/internal/state"
 )
 
@@ -16,6 +22,35 @@ func HandlerLogin(s *state.State, cmd commands.Command) error {
 
 	s.ConfigPointer.SetUser(username)
 	fmt.Printf("User has been set: %s\n", username)
+func HandlerRegister(s *state.State, cmd commands.Command) error {
+	if len(cmd.Args) == 0 {
+		return errors.New("register handler expects a username")
+	}
+	name := cmd.Args[0]
+
+	_, err := s.DB.GetUser(context.Background(), name)
+	if err == nil {
+		fmt.Printf("user '%s' already exists\n", name)
+		os.Exit(1)
+	} else if err != sql.ErrNoRows {
+		return err
+	}
+
+	user := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	}
+
+	newuser, err := s.DB.CreateUser(context.Background(), user)
+	if err != nil {
+		return fmt.Errorf("Issue creating user")
+	}
+	s.ConfigPointer.SetUser(name)
+
+	fmt.Println("User has been created")
+	printUser(newuser)
 
 	return nil
 }
